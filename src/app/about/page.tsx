7671dec89edaab69420f5b1ee7a88e91c7586cd5
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-04-13 13:39:54 +0530

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutGlobe from "@/components/about-globe";
+
+export default function AboutPage() {
+  return (
+    <>
+      <AboutGlobe />
+    </>
+  );
+}
---
+++ src/utils/links.ts
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    id: 1,
+    href: "/",
+    label: "Home",
+  },
+  {
+    id: 2,
+    href: "/about",
+    label: "About",
+  },
+  {
+    id: 3,
+    href: "/contact",
+    label: "Contact",
+  },
+];
